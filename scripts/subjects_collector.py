5ee8147ab21fd3c113ba5f1aba158d22dc398523
Dalia <ddjanati@yahoo.com>
2025-10-08 16:15:55 +0000

    Edit

--- scripts/subjects_collector.py
+++ scripts/subjects_collector.py
@@ -13,24 +13,26 @@ def main():
 
     args = parse.parse_args()
 
-    author_name = args.author.replace(" ", "%20")
-
     author1 = r"george%20orwell"
     author2 = r"donald%20trump"
 
     url1 = "https://openlibrary.org/search/authors.json?q=" + author1
-    r1 = requests.get(query_url)
+    r1 = requests.get(url1)
 
     url2 = "https://openlibrary.org/search/authors.json?q=" + author2
-    r2 = requests.get(query_url)
+    r2 = requests.get(url2)
 
-    author_data = r.json()
-    author_key = author_data["docs"][0]["key"]
+    author1_data = r1.json()
+    author2_data = r2.json()
 
     # write out raw data
-    fname = f"author_{author_key}_works.json"
+    fname = f"author1_theme.json"
     with open(os.path.join(raw_path, fname), "w") as f:
-        json.dump(books_data, f, indent=4)
+        json.dump(author1_data, f, indent=4)
+
+    fname = f"author2_theme.json"
+    with open(os.path.join(raw_path, fname), "w") as f:
+        json.dump(author2_data, f, indent=4)
 
 if __name__ == "__main__":
     main()   
